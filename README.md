beff902f960ad8b7886870e2bb39534b52a8f5ce
martavvs <marta@string.co>
2020-04-19 18:55:34 +0100

    Added the script for splitting the data into training and testing sets

--- README.md
+++ README.md
@@ -5,14 +5,13 @@ Inspiration taken from:
 https://www.pyimagesearch.com/2018/12/03/deep-learning-and-medical-image-analysis-with-keras/
 ###Dataset
 2 classe:
-- 13,794 images of infected
-- 13,794 images of unifected
+- 13,794 images of parasitized
+- 13,794 images of uninfected
 
 With a total of 27,588 images.
 You can acess the folder named 'cell_images.zip' at:
 https://lhncbc.nlm.nih.gov/publication/pub9932
 
 ### Prerequisites
-- MatPlotLib
 - Tensorflow2.0
 
---
+++ prep_data.py
@@ -0,0 +1,57 @@
+import random
+from shutil import copyfile
+from os import listdir, makedirs, walk, path
+
+#Format choosen: format contains all the images within separate folders named
+#after their respective class names
+def create_folders(dir):
+     # Set up empty folder structure for training and validation sets
+    if not path.exists(dir +'dataset/train'):
+        makedirs(dir + 'dataset/train')
+    if not path.exists(dir + 'dataset/test'):
+        makedirs(dir + 'dataset/test')
+    #create subdirectories of the 2 classes
+    new_dir = dir + 'dataset/'
+    subdirs = [subdir for subdir in listdir(new_dir)]
+    for subdir in subdirs:
+        # create label subdirectories
+        if not path.exists(new_dir + subdir + '/' +'Parasitized'):
+            makedirs(new_dir + subdir +'/Parasitized')
+        if not path.exists(new_dir + subdir + '/' +'Uninfected'):
+            makedirs(new_dir + subdir +'/Uninfected')
+
+#dir = cell_images
+def split(dir, train_size, seed):
+    #copy images to the subdirectories of the 2 classes
+    train_counter = 0
+    test_counter = 0
+    random.seed(seed)
+    subdirs = [subdir for subdir in listdir(dir) if not subdir.endswith("dataset")]
+    for subdir in subdirs:
+        dir_im = dir + subdir +'/'
+
+        for filename in listdir(dir_im):
+            if filename.endswith(".png"):
+                rand = random.uniform(0, 1)
+                if rand <= train_size:
+                    src = dir_im + filename
+                    dst = dir + 'dataset/train/' + subdir +'/' + filename
+                    copyfile(src, dst)
+                    train_counter += 1
+                else:
+                    src = dir_im + filename
+                    dst = dir + 'dataset/test/' + subdir+'/' + filename
+                    copyfile(src, dst)
+                    test_counter += 1
+    return train_counter, test_counter
+
+#To test
+if __name__ == '__main__':
+    dir = 'cell_images/'
+    train_size = 0.8
+    seed = 123
+    create_folders(dir)
+    train_counter, test_counter = split(dir, train_size, seed)
+    print('done')
+    print('Copied ' + str(train_counter) + ' images to train')
+    print('Copied ' + str(test_counter) + ' images to test')
